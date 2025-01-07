56c3da99ed1b57c4da25c7ea445fd805a1c0836d
pwcaweis <andre.weisfw@gmail.com>
2025-01-07 15:36:52 +0100

    add: onInit and onInputChange to controller
    Using GitHub Copilot with prompt: what is normally in an onInit method
    of a sapui5 app controller? how could I make use of that?

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,30 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
-  "use strict";
-  return Controller.extend("sap_taskui.controller.App", {
-    onInit: function () {
-      // Initialization code
-    },
-  });
-});
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  function (Controller, JSONModel) {
+    "use strict";
+    return Controller.extend("sap_taskui.controller.App", {
+      onInit: function () {
+        // Create a JSON model
+        var oModel = new JSONModel({
+          firstName: "",
+          lastName: "",
+        });
+        // Set the model to the view
+        this.getView().setModel(oModel);
+
+        // Attach event handler to the input fields (if needed)
+        var oFirstNameInput = this.byId("firstName");
+        var oLastNameInput = this.byId("lastName");
+
+        oFirstNameInput.attachChange(this.onInputChange, this);
+        oLastNameInput.attachChange(this.onInputChange, this);
+      },
+
+      onInputChange: function (oEvent) {
+        // Handle input change event
+        var sValue = oEvent.getParameter("value");
+        console.log("Input changed: " + sValue);
+      },
+    });
+  }
+);
